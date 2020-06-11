e9d0485e05dd8abb9047a827b3b5ce002189ec17
brian <brian@eolas>
2020-06-11 13:36:39 -0400

    [version] create version.hpp file in one cmd
    This reduces the chance that simultaneous job fails because although the
    version.hpp file exists, the VERSION variable isn't defined.

--- version.mk
+++ version.mk
@@ -8,8 +8,7 @@ VERSION := $(shell $(GIT) describe --abbrev=4 --always --dirty --tags)
 VERSION_FILE := src/version.hpp
 
 $(VERSION_FILE): .git/HEAD .git/index
-	@echo "#pragma once" > $@
-	@echo "constexpr char const* VERSION = \"$(VERSION)\";" >> $@
+	@printf "#pragma once\nconstexpr char const* VERSION = \"$(VERSION)\";" > $@
 
 # make sure the file is a dependency of all targets, so it is always
 # built and there aren't any include errors
